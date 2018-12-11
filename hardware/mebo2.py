40ba26cc9ec526bd4da7d03731cdea3b72ec2f90
Bryan Morrison <skeeter_mcbee@live.com>
2018-12-11 12:57:49 -0800

    Initial commit

---
+++ hardware/mebo2.py
@@ -0,0 +1,14 @@
+from hardware.mebo.letsrobot_to_mebo_converter import LetsRobotToMeboConverter
+from hardware.mebo.letsrobot_commands import LetsRobotCommands
+from hardware.mebo.letsrobot_to_param_lookup import letsrobot_to_param_lookup
+from hardware.mebo.handle_mebo_command import handle_mebo_command
+import hardware.mebo.mebo_constants as mebo_constants
+import httplib
+import socket
+import time
+
+def setup(robot_config):
+    return
+
+def move(args):
+    handle_mebo_command(args)
